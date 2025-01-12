2ed2f7e3dd727b94326655f8b9e6d698e8e5ad1d
FALMURADOV <faiqalmuradli00@gmail.com>
2025-01-12 17:19:08 +0400

    update

--- nn.py
+++ nn.py
@@ -1,5 +1,23 @@
-import math
 import turtle
 fnn = turtle.Turtle()
-fnn.screen.bgcolor('white')
+fnn.color('white')
+turtle.bgcolor("#73ad70")
+def curve():
+    for _ in range (200):
+        fnn.right(1)
+        fnn.forward(1)
 
+fnn.speed(4)
+
+fnn.begin_fill()
+fnn.left(90)
+fnn.left(50)
+fnn.forward(110)
+curve()
+fnn.left(120)
+curve()
+fnn.forward(110)
+fnn.end_fill()
+
+
+turtle.done()
